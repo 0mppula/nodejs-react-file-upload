ab1493974dbca1b0dbc3cb5557037d6afa3cf5a8
Omar Kraidie <omppula_1@hotmail.com>
2025-08-31 15:45:57 +0700

    Added posting images and videos via the UI.

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const colors = require('colors');
 const path = require('path');
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
